dc7e2751fec6158d64364d47e52b8b4e6062dac8
saifoelloh <saifoelloh@gmail.com>
2020-07-07 00:21:59 +0700

    change db configuration

--- config/db.js
+++ config/db.js
@@ -1,22 +1,28 @@
 const Sequelize = require('sequelize')
 
+const configuration = {
+  connectionLimit: 100,
+  host: '35.202.123.101',
+  dialect: 'mysql',
+  port: 3306,
+  operatorsAlias: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    evict: 15000,
+    idle: 20000,
+  },
+  dialectOptions: {
+    connectTimeout: 60000,
+  },
+}
+
 //configure database
-const db = new Sequelize('partner_petualang', 'root', '', {
-    connectionLimit: 100,
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    operatorsAlias: false,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        evict: 15000,
-        idle: 20000
-    },
-    dialectOptions: {
-        connectTimeout: 60000
-    }
+const db = new Sequelize('partner_petualang', 'razer', '1', {
+  ...configuration,
 })
+console.log({ ...configuration, db })
 
 module.exports = db
+
