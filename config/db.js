cc32609e90dd2a35f649d90c42b41532ccbe76da
Juliantooo <salshcrackdie@gmial.com>
2020-07-06 23:35:37 +0700

    'a'

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize')
 
 //configure database
-const db = new Sequelize('partner_petualang', 'razer', '1', {
-    host: 'localhost',
+
+const configuration = {
+    host: '35.202.123.101',
     dialect: 'mysql',
     operatorsAlias: false,
     pool: {
@@ -11,6 +12,9 @@ const db = new Sequelize('partner_petualang', 'razer', '1', {
         acquire: 30000,
         idle: 10000
     }
-})
+}
 
+console.log({ configuration })
+const db = new Sequelize('partner_petualang', 'razer', '1', { configuration })
+console.log({ ...configuration, db })
 module.exports = db
