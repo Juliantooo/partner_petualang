44a64a1f8906f886578fabad22a26f7609fa436f
Juliantooo <salshcrackdie@gmial.com>
2020-07-06 22:26:39 +0700

    bismilahhirohmanirohim

--- config/db.js
+++ config/db.js
@@ -2,8 +2,15 @@ const Sequelize = require('sequelize')
 
 //configure database
 const db = new Sequelize('partner_petualang', 'razer', '1', {
-    host: 'perciachan.xyz',
-    dialect: 'mysql'
+    host: '35.202.123.101',
+    dialect: 'mysql',
+    operatorsAlias: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 })
 
 module.exports = db
