92f9c4be9c419fb3ba00b55778fc239fb5ea4d7b
Juliantooo <salshcrackdie@gmial.com>
2020-07-06 23:49:39 +0700

    'k'

--- config/db.js
+++ config/db.js
@@ -4,13 +4,14 @@ const Sequelize = require('sequelize')
 const db = new Sequelize('partner_petualang', 'razer', '1', {
     host: '35.202.123.101',
     dialect: 'mysql',
-    port:3306,
+    port: 3306,
     operatorsAlias: false,
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
-        idle: 10000
+        evict: 15000,
+        idle: 20000
     }
 })
 
